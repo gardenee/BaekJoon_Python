9cc54865784812eead9715c60be2971cb7738d97
gardenee <snnjjang@naver.com>
2022-04-05 16:06:56 +0900

    220405

---
+++ 220405/1010.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+
+for _ in range(int(input())):
+    N, M = map(int, input().split())
+    if N == M:
+        print(1)
+    else:
+        r = M-N
+        Arr = [1] * (max(r, N+1)+1)
+        for i in range(1, (max(r, N+1)+1)):
+            Arr[i] = Arr[i-1] * i
+        ans = 0
+        i = 0
+        while i <= (r-1) and i <= N:
+            ans += (Arr[N+1]//(Arr[i+1]*Arr[N-i])) * (Arr[r-1]//(Arr[i]*Arr[r-1-i]))
+            i += 1
+        print(ans)
