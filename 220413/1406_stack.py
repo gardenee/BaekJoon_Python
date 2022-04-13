e7c8b451a19b2e4defcb3791f944091c76d42024
gardenee <snnjjang@naver.com>
2022-04-13 16:45:03 +0900

    220413

---
+++ 220413/1406_stack.py
@@ -0,0 +1,21 @@
+from sys import stdin
+
+head = list(input())
+tail = []
+
+for _ in range(int(input())):
+    command = list(stdin.readline().split())
+    if command[0] == 'L':
+        if head:
+         tail.append(head.pop())
+    elif command[0] == 'D':
+        if tail:
+            head.append(tail.pop())
+    elif command[0] == 'B':
+        if head:
+           head.pop()
+    else:
+        head.append(command[1])
+
+answer = head + tail[::-1]
+print(''.join(answer))
