69ed11b51d91d63c796705e5238fa45210de233e
gardenee <snnjjang@naver.com>
2022-04-25 11:27:53 +0900

    220425 queue

---
+++ BJ01158_queue.py
@@ -0,0 +1,23 @@
+from collections import deque
+N, K = map(int, input().split())
+
+answer = []
+alive = []
+people = deque([])
+for i in range(N):
+    people.append(i+1)
+
+T = 1; n = 0
+while n < N:
+    if not people:
+        people = deque(alive)
+        alive = []
+
+    if T < K:
+        alive.append(people.popleft())
+        T += 1
+    else:
+        answer.append(str(people.popleft()))
+        T = 1; n += 1
+
+print("<" + ", ".join(answer) + ">")
