c30fa5e63b73e7084a655297e26a4245f0c6bf63
gardenee <snnjjang@naver.com>
2022-04-30 20:37:14 +0900

    220430 queue

--- BJ19644_queue.py
+++ BJ19644_queue.py
@@ -5,24 +5,20 @@ L = int(input())
 ML, MK = map(int, input().split())
 gun = deque()
 damage = 0
-count = 0
 C = int(input())
 flag = 1
 
 for _ in range(L):
     curr = int(stdin.readline())
-    if count >= ML:
+    if gun.__len__() >= ML:
         damage -= gun.popleft()
-        count -= 1
 
     if (curr - damage - MK) <= 0:
         gun.append(MK)
         damage += MK
-        count += 1
     else:
         if C:
             gun.append(0)
-            count += 1
             C -= 1
         else:
             flag = 0
