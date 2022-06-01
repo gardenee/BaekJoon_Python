5f4c6e77ac2a41ecdab2445ea107da3d212e8b78
gardenee <snnjjang@naver.com>
2022-06-02 01:51:44 +0900

    220602

--- BJ01205.py
+++ BJ01205.py
@@ -1,11 +1,18 @@
-N, score, P = map(int, input().split())
-ranking = list(map(int, input().split()))
+from sys import stdin
 
-if
-elif ranking[-1] >= score:
-    print(-1)
+N, score, P = map(int, stdin.readline().split())
+ranking = list(map(int, stdin.readline().split()))
+
+ans = -1
+if N == P and ranking[-1] >= score: ans = -1
 else:
-    for i in range(P):
-        if ranking[i] <= score:
-            print(i+1)
-            break
+    if not ranking:
+        if P == 0: ans = -1
+        else: ans = 1
+    elif ranking[-1] > score: ans = N+1
+    else:
+        for i in range(N):
+            if ranking[i] <= score:
+                ans = i+1
+                break
+print(ans)
