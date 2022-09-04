bb48fb15fa217026be02bc50f8f2ef754a07f4e6
gardenee <snnjjang@naver.com>
2022-09-04 20:55:49 +0900

    220904

---
+++ BJ11053-2.py
@@ -0,0 +1,14 @@
+import bisect
+
+N = int(input())
+ipt = list(map(int, input().split()))
+dp = [ipt[0]]
+
+for i in range(1, N):
+    if ipt[i] > dp[-1]:
+        dp.append(ipt[i])
+    else:
+        idx = bisect.bisect_left(dp, ipt[i])
+        dp[idx] = ipt[i]
+
+print(len(dp))
