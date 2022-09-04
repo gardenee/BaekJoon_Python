ed4adcfc65cff0d11bfb7b632976e79ab5dbf5c2
gardenee <snnjjang@naver.com>
2022-09-04 18:05:37 +0900

    220904

---
+++ BJ11054.py
@@ -0,0 +1,30 @@
+import bisect
+from collections import deque
+
+N = int(input())
+ipt = list(map(int, input().split()))
+
+dp = [1] * (N+1)
+asc = deque([ipt[0]])
+desc = deque([ipt[-1]])
+answer = 1
+
+for i in range(N):
+    if ipt[i] > asc[-1]:
+        asc.append(ipt[i])
+        dp[i] = len(asc)
+    else:
+        idx = bisect.bisect_left(asc, ipt[i])
+        asc[idx] = ipt[i]
+        dp[i] = idx + 1
+
+for i in range(N-1, -1, -1):
+    if ipt[i] > desc[-1]:
+        desc.append(ipt[i])
+        dp[i] += len(desc)
+    else:
+        idx = bisect.bisect_left(desc, ipt[i])
+        desc[idx] = ipt[i]
+        dp[i] += idx + 1
+
+print(max(dp)-1)
