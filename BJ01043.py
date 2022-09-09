292658e7536f2b9bf140e808a22816184b4dc7b1
gardenee <snnjjang@naver.com>
2022-09-09 22:35:48 +0900

    220904

--- BJ01043.py
+++ BJ01043.py
@@ -1,16 +1,12 @@
 N, M = map(int, input().split())
-truth = list(map(int, input().split()))
-T = truth[0]
-truth[0] = 0
-group = dict()
-
-parents = [0] * (N+1)
+parents = []
 for i in range(N+1):
-    parents[i] = i
-    group[i] = list()
+    parents.append(i)
+groups = []
 
-for t in truth:
-    parents[t] = 0
+truth = list(map(int, input().split()))
+for i in range(1, truth[0]+1):
+    parents[truth[i]] = 0
 
 
 def findparents(a):
@@ -19,33 +15,27 @@ def findparents(a):
         return 0
     if parents[a] == a:
         return a
-    parents[a] = findparents(parents[a])
+    return findparents(parents[a])
 
 
 def union(a, b):
     global parents
-    if parents[a] == 0:
-        parents[findparents(b)] = 0
-    elif parents[b] == 0:
-        parents[findparents(a)] = 0
-    else:
-        parents[findparents(a)] = findparents(b)
+    x = findparents(a)
+    y = findparents(b)
+    parents[max(x, y)] = min(x, y)
 
 
-for j in range(M):
+for _ in range(M):
     curr = list(map(int, input().split()))
-    n = curr[0]
-    for i in range(1, n+1):
-        group[curr[i]].append(j)
-        if i == n:
+    groups.append(curr[1:])
+    for i in range(1, curr[0]):
+        union(curr[i], curr[i+1])
+
+cnt = 0
+for i in range(M):
+    for people in groups[i]:
+        if findparents(people) == 0:
+            cnt += 1
             break
-        union(curr[i+1], curr[i])
 
-result = [1] * M
-for i in range(1, N+1):
-    curr = findparents(parents[i])
-    if curr == 0:
-        for g in group[i]:
-            result[g] = 0
-
-print(sum(result))
+print(M - cnt)
