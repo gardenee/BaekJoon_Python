1bf705a865a474b98f371d2467bd7969870594ac
gardenee <snnjjang@naver.com>
2022-09-10 18:29:16 +0900

    220910

--- BJ02096.py
+++ BJ02096.py
@@ -1,1 +1,23 @@
-s
+N = int(input())
+sum1 = [0] * 3
+sum2 = [0] * 3
+
+for _ in range(N):
+    ipt = list(map(int, input().split()))
+    temp1 = [0] * 3
+    temp1[0] = min(sum1[0], sum1[1])
+    temp1[2] = min(sum1[1], sum1[2])
+    temp1[1] = min(temp1[0], temp1[2])
+    sum1 = temp1
+
+    temp2 = [0] * 3
+    temp2[0] = max(sum2[0], sum2[1])
+    temp2[2] = max(sum2[1], sum2[2])
+    temp2[1] = max(temp2[0], temp2[2])
+    sum2 = temp2
+
+    for i in range(3):
+        sum1[i] += ipt[i]
+        sum2[i] += ipt[i]
+
+print(max(sum2), min(sum1))
