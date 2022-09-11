3a0c1840a8e15adb2a7de62e889fa62d5292290a
gardenee <snnjjang@naver.com>
2022-09-12 01:08:55 +0900

    220912 two_pointer

--- BJ02467.py
+++ BJ02467.py
@@ -1,7 +1,16 @@
-import bisect
-
 N = int(input())
 reagents = list(map(int, input().split()))
-search = reagents[0] + reagents[-1]
+answer = 2000000001
+A, B = 0, 0
 
+start, end = 0, N-1
+while start < end:
+    if abs(reagents[start] + reagents[end]) < answer:
+        answer = abs(reagents[start] + reagents[end])
+        A, B = reagents[start], reagents[end]
+    if reagents[start] + reagents[end] < 0:
+        start += 1
+    else:
+        end -= 1
 
+print(A, B)
