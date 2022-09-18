91f4666aabd8aa79bc7c3b67be85519a4c21882b
gardenee <snnjjang@naver.com>
2022-09-19 00:14:35 +0900

    220919 binary_search

--- BJ01300.py
+++ BJ01300.py
@@ -1,2 +1,26 @@
 N = int(input())
 k = int(input())
+arr = []
+
+start, mid, end, cnt, answer, bool = 0, 0, N**2+1, 0, 0, False
+
+while start < end:
+    cnt = 0
+    mid = (start + end)//2
+    for i in range(N):
+        if i+1 < mid:
+            cnt += min(N, (mid-1)//(i+1))
+    print("start", start , "end", end, "mid", mid, "cnt", cnt)
+
+    if cnt <= k-1:
+        start = mid+1
+    else:
+        end = mid
+
+    if cnt == k-1:
+        bool = True
+        answer = mid
+    elif not bool and cnt < k:
+        answer = mid
+
+print(answer)
