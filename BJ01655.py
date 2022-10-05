11683f59fc83c256e6d4eef6976b6691eed46c29
gardenee <snnjjang@naver.com>
2022-10-05 23:48:18 +0900

    221005

---
+++ BJ01655.py
@@ -0,0 +1,25 @@
+from sys import stdin
+import heapq
+
+N = int(input())
+left, right = [], []
+mid, a, b = 0, 0, 0
+
+for i in range(N):
+    num = int(stdin.readline())
+    if i == 0:
+        mid = num
+    elif a >= b:
+        heapq.heappush(left, -num)
+        heapq.heappush(left, -mid)
+        heapq.heappush(right, -heapq.heappop(left))
+        mid = -heapq.heappop(left)
+        b += 1
+    else:
+        heapq.heappush(right, mid)
+        heapq.heappush(right, num)
+        heapq.heappush(left, -heapq.heappop(right))
+        mid = heapq.heappop(right)
+        a += 1
+    print(mid)
+    print(left, mid, right)
