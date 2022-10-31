9574667982a18730dbca54e83260383811b2ff1f
gardenee <snnjjang@naver.com>
2022-10-31 21:30:18 +0900

    221031

--- BJ05972.py
+++ BJ05972.py
@@ -2,17 +2,15 @@ from sys import stdin
 import heapq
 
 N, M = map(int, input().split())
-graph = dict()
+graph = [[] for _ in range(N+1)]
+
 for _ in range(M):
     A, B, C = map(int, stdin.readline().split())
-    graph.setdefault(A, [])
-    graph.setdefault(B, [])
     graph[A].append([B, C])
     graph[B].append([A, C])
 
 
 INF = 1000 * M
-visited = [False] * (N+1)
 dp = [INF] * (N+1)
 dp[1] = 0
 heap = [[0, 1]]
@@ -20,17 +18,13 @@ heap = [[0, 1]]
 
 while heap:
     price, curr = heapq.heappop(heap)
-    if curr == N:
-        break
-    if visited[curr]:
-        continue
+    if curr == N: break
+    if dp[curr] < price: continue
 
-    visited[curr] = True
     nodes = graph[curr]
-
     for node in nodes:
         n, v = node
-        if not visited[n] and price+v < dp[n]:
+        if price+v < dp[n]:
             dp[n] = price+v
             heapq.heappush(heap, [dp[n], n])
 
