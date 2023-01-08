a9d192d414ebac4efff6f6cceb7c58808fb666c2
gardenee <snnjjang@naver.com>
2023-01-08 20:49:54 +0900

    230108

--- BJ01213.py
+++ BJ01213.py
@@ -4,22 +4,22 @@ cnt_alphabet = [0] * 26
 for alphabet in name:
     cnt_alphabet[ord(alphabet)-65] += 1
 
-
 answer, center = "", ""
+impossible = "I'm Sorry Hansoo"
 
 for i in range(26):
     cnt = cnt_alphabet[i]
 
     if cnt % 2 == 1:
         if len(name) % 2 == 0 or center:
-            answer = "I'm Sorry Hansoo"
+            answer = impossible
             break
         else:
             center = chr(65+i)
 
     answer += chr(65+i) * (cnt//2)
 
-if answer != "I'm Sorry Hansoo":
+if answer != impossible:
     answer = answer + center + answer[::-1]
 
 print(answer)
