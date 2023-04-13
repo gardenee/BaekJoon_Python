c7c6411f9267458c92d1317b0a656013929ba296
gardenee <snnjjang@naver.com>
2023-04-13 21:31:26 +0900

    20230413

---
+++ BJ02343.py
@@ -0,0 +1,24 @@
+N, M = map(int, input().split())
+lectures = list(map(int, input().split()))
+
+answer = sum(lectures)
+
+start, end = max(lectures), answer+1
+while start < end:
+    mid = (start + end)//2
+
+    cnt, video = 1, 0
+    for lecture in lectures:
+        if video + lecture > mid:
+            cnt += 1
+            video = lecture
+        else:
+            video += lecture
+
+    if cnt > M:
+        start = mid+1
+    else:
+        answer = min(answer, mid)
+        end = mid
+
+print(answer)
